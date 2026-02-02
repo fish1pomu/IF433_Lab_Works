944d23107ae3810ec5c9b23dd7b3330987864db2
fish1pomu <devlin0301@gmail.com>
2026-02-02 20:14:59 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000116912_devlinValentino_week1/GameStore.kt
+++ src/oop_00000116912_devlinValentino_week1/GameStore.kt
@@ -6,3 +6,10 @@ fun main() {
     val price: Int = 600000
 
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) {
+        price - (price * 20 / 100)
+    } else {
+        price - (price * 10 / 100)
+    }
