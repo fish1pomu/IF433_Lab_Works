4c4bc4ac11e7bbca462a387d725a04e288c8adb9
fish1pomu <devlin0301@gmail.com>
2026-02-09 15:20:17 +0700

    week02: add secondary constructor

--- src/oop_00000116912_devlinvalentino_week2/student.kt
+++ src/oop_00000116912_devlinvalentino_week2/student.kt
@@ -14,4 +14,7 @@ class Student(
         }
     }
 
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
