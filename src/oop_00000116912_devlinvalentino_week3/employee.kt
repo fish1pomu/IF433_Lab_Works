c38e621a543062d77f32260722e1f63cb254d55e
fish1pomu <devlin0301@gmail.com>
2026-02-16 15:04:13 +0700

    week03: add private property

--- src/oop_00000116912_devlinvalentino_week3/employee.kt
+++ src/oop_00000116912_devlinvalentino_week3/employee.kt
@@ -1,15 +1,23 @@
 package oop_00000116912_devlinvalentino_week3
 
-class Employee(val name: String ){
+class Employee(val name: String) {
     var salary: Int = 0
-        set(value){
-            if (value < 0){
+        set(value) {
+            if (value < 0) {
                 println("eror: gaji tidak boleh negative! di set ke 0.")
-                field =0
-            }else {
-                field =value
+                field = 0
+            } else {
+                field = value
             }
-
+            println("mencoba set gaji ke: $value")
         }
-}
+    private var performanceRating: Int = 3
+    fun increasePerformance() {
+        performanceRating++
+        println("kinerja $name meningkat rating $performanceRating")
+    }
 
+    fun printStatus() {
+        println("karyawan: $name, rating: $performanceRating")
+    }
+}
