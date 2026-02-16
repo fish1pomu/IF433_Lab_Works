aa341e1e15f815b8e01d4180b255e50789790159
fish1pomu <devlin0301@gmail.com>
2026-02-16 15:47:04 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000116912_devlinvalentino_week3/player_main.kt
@@ -0,0 +1,10 @@
+package oop_00000116912_devlinvalentino_week3
+
+fun main() {
+    val player = Player("hermes")
+
+    player.addXp(50)
+    println("Current Level: ${player.level}")
+
+    player.addXp(60)
+}
