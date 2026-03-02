e960cc9d23480e3f946515954109b80d82b00ecc
fish1pomu <devlin0301@gmail.com>
2026-03-02 15:00:15 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000116912_devlinvalentino_week5/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000116912_devlinvalentino_week5
+
+class mathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
