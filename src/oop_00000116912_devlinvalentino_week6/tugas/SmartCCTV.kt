5a8014a4163b37cb0fa40025757b71f1beeecbfe
fish1pomu <devlin0301@gmail.com>
2026-03-09 16:12:55 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_00000116912_devlinvalentino_week6/tugas/SmartCCTV.kt
@@ -0,0 +1,23 @@
+package oop_00000116912_devlinvalentino_week6.tugas
+
+package oop_00000116912_devlinvalentino_week6.tugas
+
+class smartLamp(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable{
+
+    override fun turnOn() {
+        println("$name dinyalakan oleh $id.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        stopRecord()
+        println("$name dimatikan oleh $id.")
+    }
+
+    override fun startRecord() {
+        println("$name mulai merekam video ke penyimpanan lokal")
+    }
+}
