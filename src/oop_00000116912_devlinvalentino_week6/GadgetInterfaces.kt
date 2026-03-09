73a508784da731337dafee081710a108a9fed579
fish1pomu <devlin0301@gmail.com>
2026-03-09 15:30:04 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000116912_devlinvalentino_week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000116912_devlinvalentino_week6
+
+interface Camera {
+    fun turnOn(){ println("lensa kamera terbuka dan sensor aktif")}
+}
+
+interface Phone{
+    fun turnOn(){println("sinyal saluler mencari jariangan")}
+}
