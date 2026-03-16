4052ecb79bd29c1f6dcdc76eb0fc6b4e434e0685
fish1pomu <devlin0301@gmail.com>
2026-03-16 16:17:10 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000116912_devlinvalentino_week7/tugas/GameManager.kt
@@ -0,0 +1,16 @@
+package oop_00000116912_devlinvalentino_week7.tugas
+
+object gameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+
+        }
+        else {
+            isGameRunning = true
+            println("Memulai Game Engine..."))
+        }
+    }
+}
