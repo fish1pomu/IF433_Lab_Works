287dcf26082affbd8a3ecdcabfe3b64fa5dae5a4
fish1pomu <devlin0301@gmail.com>
2026-04-13 14:47:08 +0700

    week08: create nested classes for Order

---
+++ src/oop_00000116912_devlinvalentino_week8/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_00000116912_devlinvalentino_week8
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetail(val address: Address? )
+class Order (val deliveryDetail: DeliveryDetail, val totalprice: Int?)
