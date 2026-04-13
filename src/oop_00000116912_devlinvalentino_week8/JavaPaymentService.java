0b58688704f961431bb5d219acc7fb031c96fdb3
fish1pomu <devlin0301@gmail.com>
2026-04-13 15:38:05 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000116912_devlinvalentino_week8/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000116912_devlinvalentino_week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
