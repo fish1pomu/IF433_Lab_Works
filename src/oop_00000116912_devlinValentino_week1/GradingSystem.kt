f866179bea1c21b2b002bdd55b5965613607417b
fish1pomu <devlin0301@gmail.com>
2026-04-13 14:38:01 +0700

    week08: create basic UserProfile with nullable fields

--- src/oop_00000116912_devlinValentino_week1/GradingSystem.kt
+++ src/oop_00000116912_devlinValentino_week1/GradingSystem.kt
@@ -2,7 +2,7 @@ package oop_00000116912_devlinValentino_week1
 
 fun main() {
     val name: String = "john thor"
-    val score: Int = 80
+    val score: Int = 60
 
     println("Name : $name ,Nilai $score")
 
---
+++ src/oop_00000116912_devlinvalentino_week8/UserProfile.kt
@@ -0,0 +1,7 @@
+package oop_00000116912_devlinvalentino_week8
+
+class userprofile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null ,
+)
