df0535bb01188339d9fc8da012781d2b04c72d90
fish1pomu <devlin0301@gmail.com>
2026-04-27 15:53:51 +0700

    week10: (task) populate coin repository

--- src/oop_00000116912_devlinvalentino_week10/CryptoDashboard.kt
+++ src/oop_00000116912_devlinvalentino_week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000116912_devlinvalentino_week10
 
 fun main(){
     val coinRepo = walletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.125))
+    coinRepo.add(Coin("ETH", 1.5))
+    coinRepo.add(Coin("USDT", 500.0))
 }
