5dca0b5a0c8927182f3f64edee4f46c53a108936
fish1pomu <devlin0301@gmail.com>
2026-05-04 14:38:59 +0700

    week11: create string extension functions

---
+++ src/oop_00000116912_devlinvalentino_week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000116912_devlinvalentino_week11
+
+fun String.addGreeting(): String {
+    return "hallo, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
