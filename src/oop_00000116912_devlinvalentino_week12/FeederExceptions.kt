408e39bb0049824a79ccba2b37c5c0dc4de3da50
fish1pomu <devlin0301@gmail.com>
2026-05-11 15:36:05 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000116912_devlinvalentino_week12/FeederExceptions.kt
+++ src/oop_00000116912_devlinvalentino_week12/FeederExceptions.kt
@@ -6,6 +6,5 @@ FeederException(msg: String) : Exception(msg){
     class FoodEmptyException(requested: Int, available: Int) :
         FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
 
-    class DispenserJamException :
-        FeederException("Wadah dispenser tersangkut/macet!")
+    class DispenserJamException : FeederException("Wadah dispenser tersangkut/macet!")
 }
--- src/oop_00000116912_devlinvalentino_week12/SmartFeeder.kt
+++ src/oop_00000116912_devlinvalentino_week12/SmartFeeder.kt
@@ -8,6 +8,17 @@ isJammed: Boolean
     require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
+
+    if (isJammed) {
+        throw FeederException.DispenserJamException()
+    }
+
+    return if (requestedGram <= availableGram) {
+        availableGram - requestedGram
+    } else {
+        0
+    }
+
 }
 
 
