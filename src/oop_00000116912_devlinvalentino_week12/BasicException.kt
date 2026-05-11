caf65dfd5241f54d91daaa6fb3d3a8e8539ddc27
fish1pomu <devlin0301@gmail.com>
2026-05-11 15:08:03 +0700

    week12: implement try as expression

--- src/oop_00000116912_devlinvalentino_week12/BasicException.kt
+++ src/oop_00000116912_devlinvalentino_week12/BasicException.kt
@@ -10,3 +10,15 @@ fun divede(a: Int, b: Int): Int {
         println("devision attempt finished")
     }
 }
+
+fun main(){
+    println("===TEST TRY AS EXPRESSION ====")
+    val inputString = "123A"
+
+    val result: Int = try {
+        Integer.parseInt(inputString)
+    }catch (e: NumberFormatException){
+        -1
+    }
+    println("Hasil parsing: $result")
+}
