b341e9167fa33b41fe2fd6530c72c92be0b6888c
fish1pomu <devlin0301@gmail.com>
2026-05-18 15:11:11 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000116912_devlinvalentino_week13/CryptoTradeLogger.kt
@@ -0,0 +1,8 @@
+package oop_00000116912_devlinvalentino_week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double)
